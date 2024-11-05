dd0938a37a3e3001019ccbdcefddbf34aa4920da
DESKTOP-ADDHSM9\Dhruv <17bmiit075@gmail.com>
2024-11-05 10:36:38 +0530

    Environment variables and secrets

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -1,8 +1,9 @@
 import type { Schema } from "../../data/resource"
-
+import { env } from '$amplify/env/say-hello'; // the import is '$amplify/env/<function-name>'
 export const handler: Schema["sayHello"]["functionHandler"] = async (event) => {
   // arguments typed from `.arguments()`
   const { name } = event.arguments
+  const envVAr = env.NAME
   // return typed from `.returns()`
-  return `Hello, ${name}!`
+  return `Hello, ${name}, ${envVAr}!`
 }
--- amplify/functions/say-hello/resource.ts
+++ amplify/functions/say-hello/resource.ts
@@ -4,5 +4,10 @@ export const sayHello = defineFunction({
   // optionally specify a name for the Function (defaults to directory name)
   name: 'say-hello',
   // optionally specify a path to your handler (defaults to "./handler.ts")
-  entry: './handler.ts'
+  entry: './handler.ts',
+  //for  environments variables
+  environment: {
+    NAME: 'World',
+    // API_ENDPOINT: process.env.API_ENDPOINT
+  }
 });
